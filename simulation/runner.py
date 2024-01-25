85350c3d9bfba2e1333f0bc2e151000dbc847dc1
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-01-25 11:30:10 +0000

    commented out poly.xml to improve sim load time

--- simulation/runner.py
+++ simulation/runner.py
@@ -10,13 +10,17 @@ def run():
     routeInfo = traci.simulation.findRoute('-57827558', '30054903#1')
     traci.route.add('route1', routeInfo.edges)
     # traci.vehicle.add(vehID=veh_id,typeID='car', routeID='route1')
-
+    veh_id = 'custom'
     print(routeInfo)
 
 
 
     while step < 10000:
         traci.simulationStep()
+        if step == 20:
+            print("creating vehicle")
+            traci.vehicle.add(vehID=veh_id,typeID='trailer_1', routeID='route1')
+
 
 
 
